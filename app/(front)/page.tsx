c16188b5414a94a3bc484d373a1127d7de1cf101
habibkhan004 <hkhabib105@gmail.com>
2025-05-22 22:41:32 +0500

    hero section updated

--- app/(front)/page.tsx
+++ app/(front)/page.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import Hero from "@/Components/frontend/Hero";
+import { MegaMenu } from "@/Components/frontend/MegaMenu";
 export default function Home() {
   return (
-    <section className="text-3xl flex min-h-screen justify-center items-center max-w-6xl mx-auto ">
+    <section className="text-3xl  min-h-screen justify-center items-center max-w-6xl mx-auto ">
+      <div className="bg-gray-600 text-gray-50">
+        <MegaMenu></MegaMenu>
+      </div>
       <Hero />
     </section>
   );
