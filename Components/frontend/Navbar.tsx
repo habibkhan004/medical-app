c1a1e7f2ec197afd82d65334548125cc8cc01edd
habibkhan004 <hkhabib105@gmail.com>
2025-06-03 23:56:49 +0500

    Navbar changed

--- Components/frontend/Navbar.tsx
+++ Components/frontend/Navbar.tsx
@@ -19,7 +19,7 @@ export default function Navbar() {
     <header className="bg-white shadow">
       <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-3 lg:px-8">
         <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5 flex items-center gap-2">
+          <a href="/" className="-m-1.5 p-1.5 flex items-center gap-2">
             <img
               alt="Medical Logo"
               src="https://img.icons8.com/color/48/medical-doctor.png"
